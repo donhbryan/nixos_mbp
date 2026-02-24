ac3638429db3042a5c5b62cce9a1f373369c938f
donhbryan <don.h.bryan@gmail.com>
2026-02-24 10:59:38 -0600

    nix alias

--- configuration.nix
+++ configuration.nix
@@ -186,7 +186,7 @@
     # Show all logs in /var/log
     alias logs="sudo find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f"
  
- 
+	alias nix='sudo nixos-rebuild switch'
     alias ls="eza -alhM --color=auto"
     HISTTIMEFORMAT="%F %T "
     alias nala="sudo nala"
